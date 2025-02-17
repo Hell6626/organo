05b380c134f87dc40941ade1e5d9bc55d73bc69a
Hell6626 <dbrunobruno369@gmail.com>
2025-02-17 13:26:47 -0300

    a a

--- src/Componentes/Time/index.js
+++ src/Componentes/Time/index.js
@@ -1,9 +1,15 @@
 import './Time.css'
 
 const Time = (props) => {
+
+    const css = { backgroundColor: props.corSecundaria }
+
     return (
-        <section>
-            <h3>
+        <section
+            className='time'
+            style={css}
+        >
+            <h3 style={{ borderColor: props.corPrimaria }}>
                 {props.nome}
             </h3>
         </section>
