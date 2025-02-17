892e6f855095660f4bf253bc3eebea2fdd8e81f2
Hell6626 <dbrunobruno369@gmail.com>
2025-02-17 11:14:31 -0300

    a a

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { useState } from 'react'
 import Banner from './Componentes/Banner';
 import Formulario from './Componentes/Formulario';
+import Time from './Componentes/Time'
 
 function App() {
+
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador)
+    setColaboradores([...colaboradores, colaborador])
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
+      <Time nome="Programação" />
+      <Time nome="Frontend" />
+      <Time nome="Data Science" />
     </div>
   );
 }
